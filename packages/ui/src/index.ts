81dda958e0eb0b6cbecac64b69f8a88e99490ef7
Nathan Lafrance-Berger <nathan.lafranceb@gmail.com>
2026-06-23 14:13:22 -0400

    feat(ui): add new UI component library with Tailwind CSS integration and design tokens

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,42 @@
+export { Badge, badgeVariants, type BadgeProps } from "./components/ui/badge.js";
+export { Button, buttonVariants, type ButtonProps } from "./components/ui/button.js";
+export {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./components/ui/card.js";
+export {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
+  DialogTrigger,
+} from "./components/ui/dialog.js";
+export { Separator } from "./components/ui/separator.js";
+export {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetOverlay,
+  SheetPortal,
+  SheetTitle,
+  SheetTrigger,
+} from "./components/ui/sheet.js";
+export {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./components/ui/tooltip.js";
+export { cn } from "./lib/utils.js";
